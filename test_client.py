b0452745385e401bf392b74b40c70ad9fe9ef75e
Muraviev.s.d <muraviev.s.d@gmail.com>
2018-07-30 13:59:55 +0300

    изменил тест клиента

--- test_client.py
+++ test_client.py
@@ -1,13 +1,13 @@
+from my_lib_client import check_client_sys_args, client_connect
 
-from my_lib_client import check_client_sys_args,client_connect
 
 def test_check_client_sys_args():
     ip, port = check_client_sys_args()
     assert ip == '127.0.0.1'
     assert port == 7777
 
-def test_client_cconnect():
-    result = client_connect('127.0.0.1', 7777)
 
+def test_client_cconnect():
+    result, sock = client_connect('127.0.0.1', 7777)
     assert result['msg'] == 'Добро пожаловать на Наш сервер'
     assert result['action'] == 'probe'
